2e224e5eb9058d9089eee6021d4c73665b2b82e1
adithys <51487891+adithys@users.noreply.github.com>
2019-06-12 17:04:07 +0530

    Add files via upload

---
+++ C++-Task 1-Print ASCII value of character entered.cpp
@@ -0,0 +1,10 @@
+//Adithya Krishnan
+//C++ program-Task 1- To print the ASCII value of a character entered 
+#include<iostream>
+int main()
+{
+	char c;
+	std::cout<<"Character: ";
+	std::cin>>c;
+	std::cout<<"ASCII value of character: "<<int(c);
+}
